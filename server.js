dbf5beed4f810cc7d0586dea5e1bbe014956efd0
BradOFarrell <bradofarrell@gmail.com>
2017-10-18 16:12:59 -0400

    tweaked the express

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/users', UserController)
 
 app.use(express.static(__dirname+'/client/build'))
 
-app.get('/', (req,res) => {
+app.get('*', (req,res) => {
   res.sendFile(__dirname+'/client/build/index.html');
 })
 
